0c2bd80eab27cea97c80c74d720a3d8c200f652b
Milan Lajtoš <milan.lajtos@me.com>
2021-02-08 23:24:45 +0100

    centiseconds -> miliseconds

--- src/utils.tsx
+++ src/utils.tsx
@@ -19,20 +19,20 @@ export const stringToColor = (str: string) => {
   return color;
 };
 
-const $ = (n: number) => {
-  return `${n}`.padStart(2, '0');
+const $ = (n: number, padSize: number) => {
+  return `${n}`.padStart(padSize, '0');
 }
 
 // this is awful, just drop zeros and separators until you can
 export const formatTime = (t: number) => {
   const m = t / 60 | 0;
   const s = t % 60 | 0;
-  const cs = ((t - (t | 0)) * 100) | 0;
+  const ms = ((t - (t | 0)) * 1000) | 0;
 
   const parts = [];
 
   if (m > 10) {
-    parts.push($(m));
+    parts.push($(m, 2));
     parts.push(":");
   } else if (m > 0) {
     parts.push(m);
@@ -41,18 +41,18 @@ export const formatTime = (t: number) => {
 
   if (parts.length === 0) {
     if (s > 10) {
-      parts.push($(s));
+      parts.push($(s, 2));
       parts.push(".");
     } else {
       parts.push(s);
       parts.push(".");
     }
   } else {
-    parts.push($(s));
+    parts.push($(s, 2));
     parts.push(".");
   }
 
-  parts.push($(cs));
+  parts.push(<span style={{ fontSize: "0.618em", opacity: 0.8 }}>{$(ms, 3)}</span>);
 
   return (
     <span>
